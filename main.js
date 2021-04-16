7cb1eee51db43a51f8f3bba52afb035f8b34b0bf
kevAs <kevassiobo@gmail.com>
2021-04-16 19:31:50 +0200

    base

---
+++ main.js
@@ -0,0 +1,12 @@
+let array = [ "coding 11", "coding 12", "coding 13" ];
+
+console.log(array);
+
+array.push("kevin");
+
+console.log(array);
+
+array.splice(3, 1, "coding 18");
+console.log(array);
+
+array.splice(3, 1);
